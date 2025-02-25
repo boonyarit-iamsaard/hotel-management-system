27635cb74c5666753ae73d3fb19e747d3d20ca50
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-02-25 21:03:35 +0700

    build: configure trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from '~/common/components/ui/button';
+import { api } from '~/core/trpc/server';
 
-export default function Page() {
+export default async function Page() {
+  const { status } = await api.healthcheck();
+
+  console.info('status', status);
+
   return (
     <section className="grid h-[35vh] place-content-center bg-muted">
       <div className="flex w-full max-w-screen-sm flex-col items-center gap-4 p-4">
